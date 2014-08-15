cb1c9da38d5a111fcd189c484292d48cfebd19a9
Surbma <peter@surbma.hu>
2014-08-15 17:26:01 +0200

    1.0.0 - Initial release

---
+++ surbma-divi-extras.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+Plugin Name: Surbma - Divi extrák
+Description: Extra módosítások a Divi sablonhoz
+Version: 1.0.0
+Author: Surbma
+Author URI: http://surbma.hu/
+License: GPL2
+*/
+
+function surbma_divi_extras_enqueue_scripts() {
+	wp_enqueue_style( 'surbma-divi-extras', plugins_url( '', __FILE__ ) . '/surbma-divi-extras.css', false, '1.0.0' );
+}
+add_action( 'wp_enqueue_scripts', 'surbma_divi_extras_enqueue_scripts', 9999 );
+
