146bd6d8cd5c0c68dc6d4834a5ba2d1a2e4c5678
Surbma <peter@surbma.hu>
2014-11-19 12:32:10 +0100

    1.3.0
    - Added less support and compressed css

--- surbma-divi-extras.php
+++ surbma-divi-extras.php
@@ -1,16 +1,17 @@
 <?php
 
 /*
-Plugin Name: Surbma - Divi extrák
+Plugin Name: Surbma - Divi extras
+Plugin URI: http://surbma.hu/bovitmenyek/
 Description: Extra módosítások a Divi sablonhoz
-Version: 1.2.0
+Version: 1.3.0
 Author: Surbma
 Author URI: http://surbma.hu/
 License: GPL2
 */
 
 function surbma_divi_extras_enqueue_scripts() {
-	wp_enqueue_style( 'surbma-divi-extras', plugins_url( '', __FILE__ ) . '/surbma-divi-extras.css', false, '1.0.0' );
+	wp_enqueue_style( 'surbma-divi-extras', plugins_url( '', __FILE__ ) . '/surbma-divi-extras.css', false, '1.3.0' );
 }
 add_action( 'wp_enqueue_scripts', 'surbma_divi_extras_enqueue_scripts', 9999 );
 
