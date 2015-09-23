250c35ab05646c7cbd2e67fd72bb266770347e5d
Surbma <peter@surbma.hu>
2015-09-23 12:38:41 +0200

    3.0.6
    - Featured menu item position fix, if there are no other menu items.
    - Featured menu item styling in mobile menu also.
    - Compatibility check with WordPress 4.3 version.
    - Compatibility check with Divi 2.5 version.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Surbma - Divi extras
-==================
+====================
 
 Useful modifications for the Divi Theme.
--- surbma-divi-extras.php
+++ surbma-divi-extras.php
@@ -5,7 +5,7 @@ Plugin Name: Surbma - Divi Extras
 Plugin URI: http://surbma.com/wordpress-plugins/
 Description: Useful modifications for the Divi Theme.
 
-Version: 3.0.5
+Version: 3.0.6
 
 Author: Surbma
 Author URI: http://surbma.com/
@@ -17,9 +17,7 @@ Domain Path: /languages/
 */
 
 // Prevent direct access to the plugin
-if ( !defined( 'ABSPATH' ) ) {
-	die( 'Good try! :)' );
-}
+if ( !defined( 'ABSPATH' ) ) die( 'Good try! :)' );
 
 // Localization
 function surbma_divi_extras_init() {
@@ -30,18 +28,13 @@ add_action( 'plugins_loaded', 'surbma_divi_extras_init' );
 // Enqueue the css file
 function surbma_divi_extras_enqueue_scripts() {
 	if ( wp_basename( get_bloginfo( 'template_directory' ) ) == 'Divi' ) {
-		wp_enqueue_style( 'surbma-divi-extra-styles', plugins_url( '', __FILE__ ) . '/css/surbma-divi-extras.css', false, '3.0.5' );
+		wp_enqueue_style( 'surbma-divi-extra-styles', plugins_url( '', __FILE__ ) . '/css/surbma-divi-extras.css', false, '3.0.6' );
 
-		$accent_color = '#2ea3f2';
-		if ( et_get_option( 'accent_color' ) != '' )
-			$accent_color = esc_html( et_get_option( 'accent_color' ) );
+		$accent_color = esc_html( et_get_option( 'accent_color', '#2ea3f2' ) );
+		$menu_spacing = esc_attr( ceil( et_get_option( 'menu_height', '66' ) / 2 ) - 15 );
 
-		$menu_padding = 18;
-		if ( et_get_option( 'menu_height' ) != '' )
-			$menu_padding = esc_html( ceil( et_get_option( 'menu_height' ) / 2 ) - 15 );
-
-		$custom_css = "#top-menu .featured-menu a,#top-menu .featured-menu a:hover{background-color:{$accent_color};}";
-		$custom_css .= "@media only screen and (min-width: 981px){body.et_header_style_left #et-top-navigation{padding-top:{$menu_padding}px;}}";
+		$custom_css = "#main-header .featured-menu a,#main-header .featured-menu a:hover{background-color:{$accent_color} !important;}";
+		$custom_css .= "@media only screen and (min-width: 981px){body.et_header_style_left #et-top-navigation{padding-top:{$menu_spacing}px;}#main-header:not(.et-fixed-header) #top-menu .featured-menu>a{margin-bottom:{$menu_spacing}px;}}";
 
 		wp_add_inline_style( 'surbma-divi-extra-styles', $custom_css );
 	}
